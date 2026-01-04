c466df34928a9ca1149be4a51b35def995bc15b4
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2026-01-04 08:43:49 +0800

    feat(server): set up rabbitmq connection

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,16 @@
+import amqp from "amqplib";
+
 async function main() {
   console.log("Starting Peril server...");
+  const url = "amqp://guest:guest@localhost:5672/";
+  const conn = await amqp.connect(url);
+  console.log("Connected to RabbitMQ server.");
+
+  process.on("SIGINT", async () => {
+    console.log("Shutting down...");
+    await conn.close();
+    process.exit(130);
+  });
 }
 
 main().catch((err) => {
