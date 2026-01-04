2f27df4058db9a6799598d38706af8f649424f02
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2026-01-04 12:37:58 +0800

    feat(client): use transient queues for pausing

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,5 +1,34 @@
+import amqp from "amqplib";
+import { clientWelcome } from "../internal/gamelogic/gamelogic.js";
+import { declareAndBind } from "../internal/pubsub/publish.js";
+import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+
 async function main() {
-  console.log("Starting Peril client...");
+  const rabbinConnString = "amqp://guest:guest@localhost:5672/";
+  const conn = await amqp.connect(rabbinConnString);
+  console.log("Peril game client connected to RabbitMQ!");
+
+  ["SIGINT", "SIGTERM"].forEach((signal) => {
+    process.on(signal, async () => {
+      try {
+        await conn.close();
+        console.log("RabbitMQ connection closed.");
+      } catch (err) {
+        console.error("Error closing RabbitMQ connection:", err);
+      } finally {
+        process.exit(0);
+      }
+    });
+  });
+
+  const username = await clientWelcome();
+  const [channel, queue] = await declareAndBind(
+    conn,
+    ExchangePerilDirect,
+    `${PauseKey}.${username}`,
+    PauseKey,
+    "transient",
+  );
 }
 
 main().catch((err) => {
--- src/internal/pubsub/publish.ts
+++ src/internal/pubsub/publish.ts
@@ -1,4 +1,4 @@
-import type { ConfirmChannel } from "amqplib";
+import type { Channel, ChannelModel, ConfirmChannel, Replies } from "amqplib";
 
 export function publishJSON<T>(
   ch: ConfirmChannel,
@@ -23,4 +23,23 @@ export function publishJSON<T>(
       },
     );
   });
+}
+
+type SimpleQueueType = "durable" | "transient";
+
+export async function declareAndBind(
+  conn: ChannelModel,
+  exchange: string,
+  queueName: string,
+  key: string,
+  queueType: SimpleQueueType,
+): Promise<[Channel, Replies.AssertQueue]> {
+  const channel = await conn.createChannel();
+  const queue = await channel.assertQueue(queueName, {
+    durable: queueType === "durable",
+    autoDelete: queueType === "transient",
+    exclusive: queueType === "transient",
+  });
+  await channel.bindQueue(queue.queue, exchange, key);
+  return [channel, queue];
 }
